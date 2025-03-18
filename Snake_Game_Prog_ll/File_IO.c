c85393e47e2c09020fd35a4a4060a80634fdfed4
Austinfer <ozzyandrew@hotmail.com>
2025-03-18 15:01:37 -0400

    Added File I/O for high score tracking. Implemented reading/writing high scores to a file and displaying them during gameplay.

---
+++ Snake_Game_Prog_ll/File_IO.c
@@ -0,0 +1,21 @@
+// File_IO.c
+#define _CRT_SECURE_NO_WARNINGS
+#include "File_IO.h"
+
+int readHighScore() {
+    FILE* file = fopen("highscore.txt", "r");
+    int score = 0;
+    if (file) {
+        fscanf(file, "%d", &score);
+        fclose(file);
+    }
+    return score;
+}
+
+void saveHighScore(int score) {
+    FILE* file = fopen("highscore.txt", "w");
+    if (file) {
+        fprintf(file, "%d", score);
+        fclose(file);
+    }
+}
---
+++ Snake_Game_Prog_ll/File_IO.h
@@ -0,0 +1,6 @@
+// File_IO.h
+#pragma once
+#include <stdio.h>
+
+int readHighScore();
+void saveHighScore(int score);
