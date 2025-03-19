6f4abbbb5ab0b24780ae5723c1a8ca06ad6be54c
liz0x36 <livgreenwood6@gmail.com>
2025-03-19 00:14:21 -0400

    hopefully this goes on my branch but git hates me so idk. pretty much all my code and menus :p

---
+++ Snake_Game_Prog_ll/menus.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+
+#define MENU_ITEMS 3  
+
+void displayTitleArt();
+
+void displaySnakeArt();
+
+void displayStartMenu(int selected);
+
+int handleMenuSelection(void(*displayChoice)(int));
+
+int displayGameStart(int selected);
+
+void howToPlayDisplay();
+
+void scoreboardDisplay();
+
+void runMenu();
